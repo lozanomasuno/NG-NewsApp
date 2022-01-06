da570f24e876700fc04f023f88651acf74d0045b
lozanomasuno <39577534+lozanomasuno@users.noreply.github.com>
2022-01-06 13:34:08 -0500

    search Bar layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dislicores-app';
+  title: string = 'Dislicores App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    HeaderComponent,
+    SearchbarComponent    
   ],
   imports: [
     BrowserModule,
